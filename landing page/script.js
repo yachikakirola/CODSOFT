c1682507b100af16f4229055ab8e4f25e65e683f
yachikakirola <yachikakirola0@gmail.com>
2024-10-30 21:03:41 +0530

    add some changes

--- landing page/script.js
+++ landing page/script.js
@@ -3,22 +3,7 @@ function expandGrid(){
     var container = document.getElementById("griid");
     if(l==1)
     {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Audi"><img class="crs" src="data/audi.jpg" alt="audi"><div class="tover"><br>AUDI</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/BMW"><img class="crs" src="data/bmw.jpg" alt="bmw"><div class="tover"><br>BMW</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Bolero"><img class="crs" src="data/bolero.jpg" alt="bolero"><div class="tover"><br>BOLERO</div></a></div>';
-        l+=1;
-    }
-    else if(l==2)
-    {        
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Dacia_Duster"><img class="crs" src="data/duster.jpg" alt="duster"><div class="tover"><br>DUSTER</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Ferrari"><img class="crs" src="data/ferrai.jpg" alt="ferrari"><div class="tover"><br>FERRARI</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Toyota_Fortuner"><img class="crs" src="data/fortuner.jpg" alt="fortuner"><div class="tover"><br>FORTUNER</div></a></div>';
-        l+=1;
-    }
-    else if(l==3)
-    {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/MG_Motor"><img class="crs" src="data/hector.jpg" alt="hector"><div class="tover"><br>HECTOR</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Jaguar_Cars"><img class="crs" src="data/jaguar.jpg" alt="jaguar"><div class="tover"><br>JAGUAR</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Mercedes-Benz"><img class="crs" src="data/mercedes.jpg" alt="mercedes"><div class="tover"><br>MERCEDES</div></a></div>';
-        l+=1;
-    }
-    else if(l==4)
-    {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Scorpio"><img class="crs" src="data/scorpio.jpg" alt="scorpio"><div class="tover"><br>SCORPIO</div></a></div>            <div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Thar"><img class="crs" src="data/thar.jpg" alt="thar"><div class="tover"><br>THAR</div></a></div>            <div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_XUV500"><img class="crs" src="data/xuv.jpg" alt="xuv"><div class="tover"><br>XUV</div></a></div>';
+        container.innerHTML+= '<div class="box"><a href="https://images.pexels.com/photos/1038000/pexels-photo-1038000.jpeg?auto=compress&cs=tinysrgb&w=600"><img class="crs" src="data/bag.jpg" alt="bag"><div class="tover"><br>BAG</div></a></div><div class="box"><a href="https://images.pexels.com/photos/3782786/pexels-photo-3782786.jpeg?auto=compress&cs=tinysrgb&w=600"><img class="crs" src="data/heels.jpg" alt="heels"><div class="tover"><br>HEELS</div></a></div><div class="box"><a href="https://images.pexels.com/photos/860009/pexels-photo-860009.jpeg?auto=compress&cs=tinysrgb&w=600"><img class="crs" src="data/jewel.jpg" alt="jewel"><div class="tover"><br>JEWEL</div></a></div>';
         l+=1;
     }
     else
